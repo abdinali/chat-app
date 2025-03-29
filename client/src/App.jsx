4265b88fa3f9f8dde6c731090aba595f8954a589
Abdinasir Ali <131626624+abdinali@users.noreply.github.com>
2025-03-29 17:12:36 -0400

    configure app layout with react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,43 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import AppLayout from "./components/AppLayout";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Settings from "./pages/Settings";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/settings",
+        element: <Settings />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
+      },
+    ],
+  },
+]);
+
 function App() {
-	return <div>Chat App</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
