5d9b44b9cdc933f5bd5abbe44aeabd828cae1cd8
Abdinasir Ali <131626624+abdinali@users.noreply.github.com>
2025-03-30 19:33:38 -0400

    add and configure cors middleware

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 import { connectDB } from './lib/db.js';
 
@@ -15,6 +16,12 @@ const PORT = process.env.PORT || 3000;
 /* MIDDLEWARE */
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+	cors({
+		credentials: true,
+		origin: 'http://localhost:5173',
+	})
+);
 
 /* ROUTES */
 app.use('/api/auth', authRoutes);
