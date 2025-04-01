cf44b4d10a470909d5293c2519823ebef8f027c6
Abdinasir Ali <131626624+abdinali@users.noreply.github.com>
2025-04-01 17:59:47 -0400

    increase payload size

--- server/src/server.js
+++ server/src/server.js
@@ -14,7 +14,7 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 /* MIDDLEWARE */
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser());
 app.use(
 	cors({
