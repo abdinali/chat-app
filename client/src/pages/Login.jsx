4f4f759cc6bb4065d3e44a4ddd408a3960aa2e21
Abdinasir Ali <131626624+abdinali@users.noreply.github.com>
2025-04-01 14:31:03 -0400

    create login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,126 @@
+import { useState } from "react";
+import { useAuthStore } from "../store/useAuthStore";
+import { Eye, EyeOff, Loader2, Lock, Mail, MessageSquare } from "lucide-react";
+import { Link } from "react-router-dom";
+import AuthImagePattern from "../components/AuthImagePattern";
+
 function Login() {
-  return <div>Login</div>;
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { login, isLoggingIn } = useAuthStore();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    login(formData);
+  }
+
+  return (
+    <div className="grid min-h-screen lg:grid-cols-2">
+      {/* left side */}
+      <div className="flex flex-col items-center justify-center p-6 sm:p-12">
+        <div className="w-full max-w-md space-y-8">
+          {/* logo */}
+          <div className="mb-8 text-center">
+            <div className="group flex flex-col items-center gap-2">
+              <div className="bg-primary/10 group-hover:bg-primary/20 flex size-12 items-center justify-center rounded-xl transition-colors">
+                <MessageSquare className="text-primary size-6" />
+              </div>
+              <h1 className="mt-2 text-2xl font-bold">Welcome Back</h1>
+              <p className="text-base-content/60">Sign in to your account</p>
+            </div>
+          </div>
+          {/* form */}
+          <form onSubmit={handleSubmit} className="space-y-6">
+            {/* EMAIL */}
+            <div className="form-control">
+              <label htmlFor="email" className="label my-2">
+                <span className="label-text font-medium">Email</span>
+              </label>
+              <div className="relative">
+                <div className="pointer-events-none absolute inset-y-0 left-0 z-50 flex items-center pl-3">
+                  <Mail className="text-base-content/80 size-5" />
+                </div>
+                <input
+                  id="email"
+                  type="email"
+                  className={`input input-bordered w-full pl-10`}
+                  placeholder="you@example.com"
+                  value={formData.email}
+                  onChange={(e) =>
+                    setFormData({ ...formData, email: e.target.value })
+                  }
+                />
+              </div>
+            </div>
+            {/* PASSWORD */}
+            <div className="form-control">
+              <label htmlFor="password" className="label my-2">
+                <span className="label-text font-medium">Password</span>
+              </label>
+              <div className="relative">
+                <div className="pointer-events-none absolute inset-y-0 left-0 z-50 flex items-center pl-3">
+                  <Lock className="text-base-content/80 size-5" />
+                </div>
+                <input
+                  id="password"
+                  type={showPassword ? "text" : "password"}
+                  className={`input input-bordered w-full pl-10`}
+                  placeholder="••••••••"
+                  value={formData.password}
+                  onChange={(e) =>
+                    setFormData({ ...formData, password: e.target.value })
+                  }
+                />
+                <button
+                  type="button"
+                  className="absolute inset-y-0 right-0 flex items-center pr-3"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? (
+                    <Eye className="text-base-content/40 size-5" />
+                  ) : (
+                    <EyeOff className="text-base-content/40 size-5" />
+                  )}
+                </button>
+              </div>
+            </div>
+            {/* SUBMIT BUTTON */}
+            <button
+              type="submit"
+              className="btn btn-primary w-full"
+              disabled={isLoggingIn}
+            >
+              {isLoggingIn ? (
+                <>
+                  <Loader2 className="size-5 animate-spin" /> Authenticating...
+                </>
+              ) : (
+                "Sign in"
+              )}
+            </button>
+          </form>
+          {/* LINK TO LOGIN PAGE */}
+          <div className="text-center">
+            <p className="text-base-content/40">
+              Don&apos;t have an account?{" "}
+              <Link to="/signup" className="link link-primary">
+                Sign up
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* right side */}
+      <AuthImagePattern
+        title="Join our community"
+        subtitle="Connect with friends, share moments, and stay in touch with your loved ones."
+      />
+    </div>
+  );
 }
 
 export default Login;
