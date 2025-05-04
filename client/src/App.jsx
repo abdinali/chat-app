8943083f999d456b5d21eb4e203f93d3f33465e4
Abdinasir Ali <131626624+abdinali@users.noreply.github.com>
2025-05-04 13:04:12 -0400

    implement ui theme selection feature in settings page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Profile from "./pages/Profile";
 import Settings from "./pages/Settings";
 
 import { useAuthStore } from "./store/useAuthStore";
+import { useThemeStore } from "./store/useThemeStore";
 import { useEffect } from "react";
 
 import { Loader } from "lucide-react";
@@ -15,6 +16,7 @@ import { Toaster } from "react-hot-toast";
 
 function App() {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+  const { theme } = useThemeStore();
 
   useEffect(function () {
     checkAuth();
@@ -30,28 +32,30 @@ function App() {
 
   return (
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route
-          path="/"
-          element={authUser ? <Home /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/profile"
-          element={authUser ? <Profile /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/signup"
-          element={!authUser ? <Signup /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/login"
-          element={!authUser ? <Login /> : <Navigate to="/" />}
-        />
-        <Route path="/settings" element={<Settings />} />
-      </Routes>
+      <div data-theme={theme}>
+        <Navbar />
+        <Routes>
+          <Route
+            path="/"
+            element={authUser ? <Home /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/profile"
+            element={authUser ? <Profile /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/signup"
+            element={!authUser ? <Signup /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/login"
+            element={!authUser ? <Login /> : <Navigate to="/" />}
+          />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
 
-      <Toaster />
+        <Toaster />
+      </div>
     </BrowserRouter>
   );
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -23,6 +23,7 @@ function Navbar() {
           <div className="flex items-center gap-2">
             <Link to="/settings" className="btn btn-sm gap-2 transition-colors">
               <Settings className="size-4" />
+              <span className="hidden sm:inline">Settings</span>
             </Link>
             {authUser && (
               <>
